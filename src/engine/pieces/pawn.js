44af48355772a344794417ee3f2f1933d5d07f9a
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-22 16:08:00 +0000

    refactor: refactoring the code by changing the if..else to ternary expression

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,38 +9,37 @@ export default class Pawn extends Piece {
 
   getAvailableMoves(board) {
     let location = board.findPiece(this);
-    const currentRow = location.row;
-    
+    const getTwoMoves = (player) => {
+      return player === "white"
+        ? [
+            Square.at(location.row + 1, location.col),
+            Square.at(location.row + 2, location.col),
+          ]
+        : [
+            Square.at(location.row - 1, location.col),
+            Square.at(location.row - 2, location.col),
+          ];
+    };
+    const getOneMove = (player) => {
+      return player === "white"
+        ? [Square.at(location.row + 1, location.col)]
+        : [Square.at(location.row - 1, location.col)];
+    };
     if (this.player === Player.WHITE) {
       //Pawn can move one or two spaces if it is their first move i.e., at the initial position
       // initial position for the Wite pawns is row=1
-      if (currentRow === 1) {
-        return [
-          Square.at(currentRow + 1, location.col),
-          Square.at(currentRow + 2, location.col),
-        ];
-      } else {
-        return [Square.at(currentRow + 1, location.col)];
-      }
-    }
-    if (this.player === Player.BLACK) {
-        //Pawn can move one or two spaces if it is their first move i.e., at the initial position
+      return location.row === 1 ? getTwoMoves("white") : getOneMove("white");
+    } else {
+      //Pawn can move one or two spaces if it is their first move i.e., at the initial position
       // initial position for the Black pawns is row=6
-      if (currentRow === 6) {
-        return [
-          Square.at(currentRow - 1, location.col),
-          Square.at(currentRow - 2, location.col),
-        ];
-      } else {
-        return [Square.at(location.row - 1, location.col)];
-      }
+      return location.row === 6 ? getTwoMoves("black") : getOneMove("black");
     }
   }
 }
 
 /* Initial Old Code */
-    // if (this.player === Player.WHITE) {
-    //     return Square.at(location.row + 1, location.col)
-    // } else {
-    //     return Square.at(location.row - 1, location.col)
-    // }
+// if (this.player === Player.WHITE) {
+//     return Square.at(location.row + 1, location.col)
+// } else {
+//     return Square.at(location.row - 1, location.col)
+// }
