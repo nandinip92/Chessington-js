254dcc7c2dadccb5c94f55d4c1e03fa95868397e
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-23 17:29:37 +0000

    refactor: renaming getMoveSquare() to getSquareSpac() and removing unnecessary comments

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -35,7 +35,7 @@ export default class Pawn extends Piece {
         frontTwoSteps: { row: -2, col: 0 },
       };
     }
-    const getMoveSquare = (direction) => {
+    const getSquareSpace = (direction) => {
       const newRow = location.row + direction.row;
       const newCol = location.col + direction.col;
       return [Square.at(newRow, newCol)];
@@ -45,13 +45,13 @@ export default class Pawn extends Piece {
     //for col 1 it can only move  to its diagonal left
     let diagonalMoves;
     if (location.col === 0)
-      diagonalMoves = getMoveSquare(pawnDirections.dright);
+      diagonalMoves = getSquareSpace(pawnDirections.dright);
     else if (location === 7)
-      diagonalMoves = getMoveSquare(pawnDirections.dleft);
+      diagonalMoves = getSquareSpace(pawnDirections.dleft);
     else
       diagonalMoves = [
-        ...getMoveSquare(pawnDirections.dright),
-        ...getMoveSquare(pawnDirections.dleft),
+        ...getSquareSpace(pawnDirections.dright),
+        ...getSquareSpace(pawnDirections.dleft),
       ];
 
     const opponentPeice =
@@ -73,32 +73,25 @@ export default class Pawn extends Piece {
     if (validDiagonalMoves.length !== 0) return validDiagonalMoves;
 
     const getTwoMoves = () => {
-      const firstMove = getMoveSquare(pawnDirections.frontOneStep);
+      const firstMove = getSquareSpace(pawnDirections.frontOneStep);
       return board.getPiece(...firstMove) !== undefined
         ? []
-        : [...firstMove, ...getMoveSquare(pawnDirections.frontTwoSteps)];
+        : [...firstMove, ...getSquareSpace(pawnDirections.frontTwoSteps)];
     };
 
     //Pawn can move one or two spaces if it is their first move i.e., at the initial position
     // initial position for the Wite pawns is row=1
     // initial position for the Black pawns is row=6
     const pawnForwardMoves =
-      location.row === 1 || location.row === 6 ? getTwoMoves() : getMoveSquare(pawnDirections.frontOneStep);
-    
+      location.row === 1 || location.row === 6
+        ? getTwoMoves()
+        : getSquareSpace(pawnDirections.frontOneStep);
+
     // Check if the square is empty, if it is then it is a valid move
-    const availableMoves = pawnForwardMoves.filter(loc=> board.getPiece(loc)===undefined);
+    const availableMoves = pawnForwardMoves.filter(
+      (loc) => board.getPiece(loc) === undefined
+    );
     return availableMoves;
   }
 }
 
-/* Initial Old Code */
-
-// if (this.player === Player.WHITE) {
-//   //Pawn can move one or two spaces if it is their first move i.e., at the initial position
-//   // initial position for the Wite pawns is row=1
-//   return location.row === 1 ? getTwoMoves() : getOneMove();
-// } else {
-//   //Pawn can move one or two spaces if it is their first move i.e., at the initial position
-//   // initial position for the Black pawns is row=6
-//   return location.row === 6 ? getTwoMoves() : getOneMove();
-// }
