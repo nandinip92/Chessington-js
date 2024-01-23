22faed4f552e0b5ae272e8f4e959d839c1291137
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-23 11:00:00 +0000

    fix: changing to strict equality

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,28 +1,27 @@
-import Square from '../square';
-import Piece from './piece';
-import GameSettings from '../gameSettings';
+import Square from "../square";
+import Piece from "./piece";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        //return new Array(0);
-        const location = board.findPiece(this);
-        let availableMoves = [];
+  getAvailableMoves(board) {
+    //return new Array(0);
+    const location = board.findPiece(this);
+    let availableMoves = [];
 
-        
-        for(let i=0; i<GameSettings.BOARD_SIZE;i++){
-            //Get Horizontal Moves
-            if(i!=location.col){
-                availableMoves.push(Square.at(location.row, i));
-            }
-            //Get Vertical Moves
-            if(i!=location.row){
-                availableMoves.push(Square.at(i, location.col));
-            }
-        }
-        return availableMoves;
+    for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+      //Get Horizontal Moves
+      if (i !== location.col) {
+        availableMoves.push(Square.at(location.row, i));
+      }
+      //Get Vertical Moves
+      if (i !== location.row) {
+        availableMoves.push(Square.at(i, location.col));
+      }
     }
+    return availableMoves;
+  }
 }
