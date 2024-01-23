6f68fa9e955ae6996b9de6d4c103344c31099982
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-23 17:27:09 +0000

    refactor: Refactoring the code for the move one step and two steps accordingly using the 'pawnDirections'

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -19,18 +19,20 @@ export default class Pawn extends Piece {
     )
       return [];
 
-    let pawnMoveDirections;
+    let pawnDirections;
     if (this.player === Player.WHITE) {
-      pawnMoveDirections = {
+      pawnDirections = {
         dright: { row: 1, col: 1 },
         dleft: { row: 1, col: -1 },
-        front: { row: 1, col: 0 },
+        frontOneStep: { row: 1, col: 0 },
+        frontTwoSteps: { row: 2, col: 0 },
       };
     } else {
-      pawnMoveDirections = {
+      pawnDirections = {
         dright: { row: -1, col: 1 },
         dleft: { row: -1, col: -1 },
-        front: { row: -1, col: 0 },
+        frontOneStep: { row: -1, col: 0 },
+        frontTwoSteps: { row: -2, col: 0 },
       };
     }
     const getMoveSquare = (direction) => {
@@ -43,13 +45,13 @@ export default class Pawn extends Piece {
     //for col 1 it can only move  to its diagonal left
     let diagonalMoves;
     if (location.col === 0)
-      diagonalMoves = getMoveSquare(pawnMoveDirections.dright);
+      diagonalMoves = getMoveSquare(pawnDirections.dright);
     else if (location === 7)
-      diagonalMoves = getMoveSquare(pawnMoveDirections.dleft);
+      diagonalMoves = getMoveSquare(pawnDirections.dleft);
     else
       diagonalMoves = [
-        ...getMoveSquare(pawnMoveDirections.dright),
-        ...getMoveSquare(pawnMoveDirections.dleft),
+        ...getMoveSquare(pawnDirections.dright),
+        ...getMoveSquare(pawnDirections.dleft),
       ];
 
     const opponentPeice =
@@ -71,38 +73,20 @@ export default class Pawn extends Piece {
     if (validDiagonalMoves.length !== 0) return validDiagonalMoves;
 
     const getTwoMoves = () => {
-      let firstMove, secondMove;
-      if (this.player === Player.WHITE) {
-        firstMove = Square.at(location.row + 1, location.col);
-        secondMove = Square.at(location.row + 2, location.col);
-      } else {
-        firstMove = Square.at(location.row - 1, location.col);
-        secondMove = Square.at(location.row - 2, location.col);
-      }
-      //If the firstMove is allowed then only the pawn can move
-      return board.getPiece(firstMove) !== undefined
+      const firstMove = getMoveSquare(pawnDirections.frontOneStep);
+      return board.getPiece(...firstMove) !== undefined
         ? []
-        : [firstMove, secondMove];
-    };
-    const getOneMove = () => {
-      return this.player === Player.WHITE
-        ? [Square.at(location.row + 1, location.col)]
-        : [Square.at(location.row - 1, location.col)];
+        : [...firstMove, ...getMoveSquare(pawnDirections.frontTwoSteps)];
     };
 
     //Pawn can move one or two spaces if it is their first move i.e., at the initial position
     // initial position for the Wite pawns is row=1
     // initial position for the Black pawns is row=6
-    const pawnMoves =
-      (location.row === 1 && this.player === Player.WHITE) ||
-      (location.row === 6 && this.player === Player.BLACK)
-        ? getTwoMoves()
-        : getOneMove();
-    //console.log("PawnMoves-->", pawnMoves);
-    const availableMoves = pawnMoves.filter(
-      (loc) => board.getPiece(loc) === undefined
-    );
-    //console.log("availableMoves--->", availableMoves);
+    const pawnForwardMoves =
+      location.row === 1 || location.row === 6 ? getTwoMoves() : getMoveSquare(pawnDirections.frontOneStep);
+    
+    // Check if the square is empty, if it is then it is a valid move
+    const availableMoves = pawnForwardMoves.filter(loc=> board.getPiece(loc)===undefined);
     return availableMoves;
   }
 }
