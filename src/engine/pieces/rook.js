0d71f4b1252d85fcd77052152b8d5fce2638de0a
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-23 10:35:48 +0000

    feat: Adding code to get the available moves for the Rook verlical and horizontal

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
+import Square from '../square';
 import Piece from './piece';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,21 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        //return new Array(0);
+        const location = board.findPiece(this);
+        let availableMoves = [];
+
+        
+        for(let i=0; i<GameSettings.BOARD_SIZE;i++){
+            //Get Horizontal Moves
+            if(i!=location.col){
+                availableMoves.push(Square.at(location.row, i));
+            }
+            //Get Vertical Moves
+            if(i!=location.row){
+                availableMoves.push(Square.at(i, location.col));
+            }
+        }
+        return availableMoves;
     }
 }
