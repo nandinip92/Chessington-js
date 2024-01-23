3075d91c484e1ba086bd3ede7830cfe36262a3fa
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-23 16:03:47 +0000

    feat: Adding code to move the pawn to diagonal, and donot move if the diagonal piece is King and donot move the pawn past the board

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Player from "../player";
 import Square from "../square";
+import King from "./king";
 import Piece from "./piece";
 
 export default class Pawn extends Piece {
@@ -9,6 +10,60 @@ export default class Pawn extends Piece {
 
   getAvailableMoves(board) {
     let location = board.findPiece(this);
+
+    //Check if the pawns have reached their respective ends on the board
+    // i.e, for Whites = 7 and for Blacks = 0
+    if (
+      (location.row === 7 && this.player === Player.WHITE) ||
+      (location.row === 0 && this.player === Player.BLACK)
+    )
+      return [];
+
+    let diagonalMove;
+    let opponentPeice = undefined;
+    if (this.player === Player.WHITE) {
+      // Pawn can move diagonally in two directions if it is between col 1-6
+      // for col 0 it can only move to its diagonal right
+      //for col 1 it can only move  to its diagonal left
+      const diagonalRight = Square.at(location.row + 1, location.col + 1);
+      const diagonalLeft = Square.at(location.row + 1, location.col - 1);
+      if (location.col === 0) diagonalMove = [diagonalRight];
+      else if (location.col === 7) diagonalMove = [diagonalLeft];
+      else diagonalMove = [diagonalRight, diagonalLeft];
+      opponentPeice = Player.BLACK;
+    } else {
+      const diagonalRight = Square.at(location.row - 1, location.col + 1);
+      const diagonalLeft = Square.at(location.row - 1, location.col - 1);
+      if (location.col === 0) diagonalMove = [diagonalRight];
+      else if (location.col === 7) diagonalMove = [diagonalLeft];
+      else diagonalMove = [diagonalRight, diagonalLeft];
+      opponentPeice = Player.WHITE;
+    }
+
+    // This diagonal  Move is only valid if there is an Opposing peice present in that square
+    // Checking if the diagonal move has the opposing peice and opposing peice should not be King
+    const checkPeice = (loc) => {
+      const peice = board.getPiece(loc);
+
+      if (
+        peice !== undefined &&
+        peice.player == opponentPeice &&
+        ! (peice instanceof King)
+      ) {
+        return peice;
+      }
+    };
+    const diagonalsWithOpposingPiece = diagonalMove.filter((loc) =>
+      checkPeice(loc)
+    );
+
+    if (diagonalsWithOpposingPiece.length !== 0)
+      return diagonalsWithOpposingPiece;
+    //console.log("----->", diagonalsWithOpposingPiece);
+    // console.log(board.getPiece(Square.at(4, 4)));
+    // const x = board.getPiece(Square.at(4, 4));
+    // console.log(x.player === Player.BLACK);
+
     const getTwoMoves = () => {
       let firstMove, secondMove;
       if (this.player === Player.WHITE) {
