9f8a839f16e2c2ab8a8a1ae875992bd6f8881dd8
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-24 13:22:26 +0000

    feat: Implementing code to take on the opposing pieces and not opposing King

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,6 +1,7 @@
 import GameSettings from "../gameSettings";
 import Player from "../player";
 import Square from "../square";
+import King from "./king";
 import Piece from "./piece";
 
 export default class Bishop extends Piece {
@@ -21,48 +22,80 @@ export default class Bishop extends Piece {
       );
     };
     let bishopMoves = [];
+    const oppositePiece =
+      this.player === Player.WHITE ? Player.BLACK : Player.WHITE;
     //Iterating diagonally towards top-right topRight: { row: +1, col: +1 }
     // Bishop pass through friendly pieces or opposing pieces
-
+    // Bishop can take opposing pieces
     for (let i = 1; isValidMove(currentRow + i, currentCol + i); i++) {
       const newRow = currentRow + i;
       const newCol = currentCol + i;
       const newSquare = Square.at(newRow, newCol);
       const boardPiece = board.getPiece(newSquare);
       if (boardPiece === undefined) bishopMoves.push(newSquare);
-      else break;
+      else {
+        if (
+          boardPiece.player === oppositePiece &&
+          !(boardPiece instanceof King)
+        )
+          bishopMoves.push(newSquare);
+        break;
+      }
     }
 
     //Iterating diagonally towards top-left topRight: { row: +1, col: -1 }
     // Bishop pass through friendly pieces or opposing pieces
+    // Bishop can take opposing pieces
     for (let i = 1; isValidMove(currentRow + i, currentCol - i); i++) {
       const newRow = currentRow + i;
       const newCol = currentCol - i;
       const newSquare = Square.at(newRow, newCol);
       const boardPiece = board.getPiece(newSquare);
       if (boardPiece === undefined) bishopMoves.push(newSquare);
-      else break;
+      else {
+        if (
+          boardPiece.player === oppositePiece &&
+          !(boardPiece instanceof King)
+        )
+          bishopMoves.push(newSquare);
+        break;
+      }
     }
     // Iterating diagonally towards top-left bottomRight: { row: -1, col: +1 }
     // Bishop pass through friendly pieces or opposing pieces
+    // Bishop can take opposing pieces
     for (let i = 1; isValidMove(currentRow - i, currentCol + i); i++) {
       const newRow = currentRow - i;
       const newCol = currentCol + i;
       const newSquare = Square.at(newRow, newCol);
       const boardPiece = board.getPiece(newSquare);
       if (boardPiece === undefined) bishopMoves.push(newSquare);
-      else break;
+      else {
+        if (
+          boardPiece.player === oppositePiece &&
+          !(boardPiece instanceof King)
+        )
+          bishopMoves.push(newSquare);
+        break;
+      }
     }
     //Iterating diagonally towards top-left bottomRight: { row: -1, col: -1 }
     // Bishop pass through friendly pieces or opposing pieces
-
+    // Bishop can take opposing pieces
     for (let i = 1; isValidMove(currentRow - i, currentCol - i); i++) {
       const newRow = currentRow - i;
       const newCol = currentCol - i;
       const newSquare = Square.at(newRow, newCol);
       const boardPiece = board.getPiece(newSquare);
       if (boardPiece === undefined) bishopMoves.push(newSquare);
-      else break;
+      else {
+        if (
+          boardPiece.player === oppositePiece &&
+          !(boardPiece instanceof King)
+        )
+          bishopMoves.push(newSquare);
+        break;
+      }
     }
     return bishopMoves;
   }
