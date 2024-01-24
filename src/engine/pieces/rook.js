83579ae1124af318a0eb2ae7bef810b0f86504cd
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-24 10:19:19 +0000

    feat: Implementing logic that Rook can take the opposing pieces and cannot take the opposing King

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,6 +1,8 @@
 import Square from "../square";
 import Piece from "./piece";
 import GameSettings from "../gameSettings";
+import Player from "../player";
+import King from "./king";
 
 export default class Rook extends Piece {
   constructor(player) {
@@ -12,7 +14,9 @@ export default class Rook extends Piece {
     const location = board.findPiece(this);
     const currentRow = location.row;
     const currentCol = location.col;
-    
+    const opponentPiece =
+      this.player === Player.WHITE ? Player.BLACK : Player.WHITE;
+
     //Rook Should not move through friendly pieces and opposite pieces
     const getForwardMoves = (direction) => {
       const forwardMoves = [];
@@ -22,8 +26,13 @@ export default class Rook extends Piece {
           direction === "V"
             ? Square.at(i, currentCol)
             : Square.at(currentRow, i);
+
         if (board.getPiece(rookMove) === undefined) forwardMoves.push(rookMove);
-        else break;
+        else {
+          const boardPiece = board.getPiece(rookMove);
+          if (boardPiece.player === opponentPiece && !(boardPiece instanceof King)) forwardMoves.push(rookMove);
+          break;
+        }
       }
       return forwardMoves;
     };
@@ -38,12 +47,14 @@ export default class Rook extends Piece {
             : Square.at(currentRow, i);
         if (board.getPiece(rookMove) === undefined)
           backwardMoves.push(rookMove);
-        else break;
+          else{
+            const boardPiece = board.getPiece(rookMove);
+            if(boardPiece.player===opponentPiece && !(boardPiece instanceof King)) backwardMoves.push(rookMove);
+            break;
+          };
       }
       return backwardMoves;
     };
-
-    
 
     const verticalRookMoves = [
       ...getForwardMoves("V"),
@@ -61,6 +72,7 @@ export default class Rook extends Piece {
 }
 
 /*Following is  Horizontal and Vertical Moves logic clearly with different for loops*/
+//Rook Should not move through friendly pieces and opposite pieces
 // let horizontalRookMoves = [],
 //   verticalRookMoves = [];
 // for (let i = currentRow + 1; i < GameSettings.BOARD_SIZE; i++) {
