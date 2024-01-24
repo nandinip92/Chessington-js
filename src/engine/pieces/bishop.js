53752125604e3151680430b6639ce3c351777169
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-24 13:16:39 +0000

    refactor: adding comments

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -10,48 +10,59 @@ export default class Bishop extends Piece {
 
   getAvailableMoves(board) {
     const location = board.findPiece(this);
-    const currentRow=location.row;
-    const currentCol=location.col;
-    const isValidMove=(row,col)=>{
-        return row>=0&&row<GameSettings.BOARD_SIZE&&col>=0&&col<GameSettings.BOARD_SIZE;
-    }
-    let bishopMoves=[];
+    const currentRow = location.row;
+    const currentCol = location.col;
+    const isValidMove = (row, col) => {
+      return (
+        row >= 0 &&
+        row < GameSettings.BOARD_SIZE &&
+        col >= 0 &&
+        col < GameSettings.BOARD_SIZE
+      );
+    };
+    let bishopMoves = [];
     //Iterating diagonally towards top-right topRight: { row: +1, col: +1 }
-    for(let i =1;isValidMove(currentRow+i,currentCol+i);i++){
-        const newRow = currentRow+i;
-        const newCol = currentCol+i;
-        const newSquare = Square.at(newRow,newCol);
-        const boardPiece = board.getPiece(newSquare)
-        if(boardPiece===undefined) bishopMoves.push(newSquare);
-        else break;
+    // Bishop pass through friendly pieces or opposing pieces
+
+    for (let i = 1; isValidMove(currentRow + i, currentCol + i); i++) {
+      const newRow = currentRow + i;
+      const newCol = currentCol + i;
+      const newSquare = Square.at(newRow, newCol);
+      const boardPiece = board.getPiece(newSquare);
+      if (boardPiece === undefined) bishopMoves.push(newSquare);
+      else break;
     }
 
-     //Iterating diagonally towards top-left topRight: { row: +1, col: -1 }
-     for(let i =1;isValidMove(currentRow+i,currentCol-i);i++){
-        const newRow = currentRow+i;
-        const newCol = currentCol-i;
-        const newSquare = Square.at(newRow,newCol);
-        const boardPiece = board.getPiece(newSquare)
-        if(boardPiece===undefined) bishopMoves.push(newSquare);
-        else break;
+    //Iterating diagonally towards top-left topRight: { row: +1, col: -1 }
+    // Bishop pass through friendly pieces or opposing pieces
+    for (let i = 1; isValidMove(currentRow + i, currentCol - i); i++) {
+      const newRow = currentRow + i;
+      const newCol = currentCol - i;
+      const newSquare = Square.at(newRow, newCol);
+      const boardPiece = board.getPiece(newSquare);
+      if (boardPiece === undefined) bishopMoves.push(newSquare);
+      else break;
     }
-     //Iterating diagonally towards top-left bottomRight: { row: -1, col: +1 }
-     for(let i =1;isValidMove(currentRow-i,currentCol+i);i++){
-        const newRow = currentRow-i;
-        const newCol = currentCol+i;
-        const newSquare = Square.at(newRow,newCol);
-        const boardPiece = board.getPiece(newSquare)
-        if(boardPiece===undefined) bishopMoves.push(newSquare);
-        else break;
+    // Iterating diagonally towards top-left bottomRight: { row: -1, col: +1 }
+    // Bishop pass through friendly pieces or opposing pieces
+    for (let i = 1; isValidMove(currentRow - i, currentCol + i); i++) {
+      const newRow = currentRow - i;
+      const newCol = currentCol + i;
+      const newSquare = Square.at(newRow, newCol);
+      const boardPiece = board.getPiece(newSquare);
+      if (boardPiece === undefined) bishopMoves.push(newSquare);
+      else break;
     }
     //Iterating diagonally towards top-left bottomRight: { row: -1, col: -1 }
-    for(let i =1;isValidMove(currentRow-i,currentCol-i);i++){
-        const newRow = currentRow-i;
-        const newCol = currentCol-i;
-        const newSquare = Square.at(newRow,newCol);
-        const boardPiece = board.getPiece(newSquare)
-        if(boardPiece===undefined) bishopMoves.push(newSquare);
-        else break;
+    // Bishop pass through friendly pieces or opposing pieces
+
+    for (let i = 1; isValidMove(currentRow - i, currentCol - i); i++) {
+      const newRow = currentRow - i;
+      const newCol = currentCol - i;
+      const newSquare = Square.at(newRow, newCol);
+      const boardPiece = board.getPiece(newSquare);
+      if (boardPiece === undefined) bishopMoves.push(newSquare);
+      else break;
     }
     return bishopMoves;
   }
